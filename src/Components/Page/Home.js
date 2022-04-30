2f008c366a425bad4b28c15381137c1604b03bde
“Allen” <alnwangchi@gmail.com>
2022-05-01 04:00:49 +0800

    首頁

--- src/Components/Page/Home.js
+++ src/Components/Page/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import SwiperCarousel from "../SwiperCarousel";
+import { Container } from "@mui/material";
 
 function Home() {
-    return <div>首頁</div>;
+    return (
+        <Container>
+            <SwiperCarousel />
+            <iframe
+                src="https://www.youtube.com/embed/cuYkp5B8Lkw"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                style={{ width: "100%", aspectRatio: "12/5" }}
+            ></iframe>
+        </Container>
+    );
 }
 
 export default Home;
