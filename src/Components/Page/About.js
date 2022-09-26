db0a9cc03ec7e9064f7ede0fd5681a2ad9f5851b
LAPTOP-3V4UE3CM\user <fubini4817@gmail.com>
2022-09-26 17:40:39 +0800

    關於我&常見問題 wording 調整

--- src/Components/Page/About.js
+++ src/Components/Page/About.js
@@ -4,8 +4,19 @@ import Headshot from "../../Asset/Image/headshot.png";
 import Title from "../Typography/Title";
 import { styled } from "@mui/material/styles";
 
-const Content = styled("div")({
+const Content = styled("div")(({ theme }) => ({
     display: "flex",
+    [theme.breakpoints.down("sm")]: {
+        flexDirection: "column",
+        alignItems: "center",
+    },
+}));
+
+const IndentDiv = styled("div")({
+    marginLeft: "48px",
+});
+const IndentDiv2 = styled("div")({
+    marginLeft: "80px",
 });
 
 const HeadshotPicture = styled("img")({
@@ -17,7 +28,13 @@ const HeadshotPicture = styled("img")({
     backgroundPosition: "center",
 });
 
-const Introduction = styled("div")({ flex: 1, padding: "0px 20px" });
+const Introduction = styled("div")(({ theme }) => ({
+    flex: 1,
+    padding: "0px 20px",
+    [theme.breakpoints.down("sm")]: {
+        marginTop: "10px",
+    },
+}));
 
 function About() {
     return (
@@ -26,12 +43,23 @@ function About() {
             <Content>
                 <HeadshotPicture />
                 <Introduction>
-                    Hi ! 我是蔡佩彤 <br />
+                    Hi !
+                    我是Jamie老師，本身從事化妝保養品牌以及護膚服務多年，想幫助客戶們無懼素顏
+                    、 感受美麗的每一天。
                     <br />
-                    經歷：高雄巨蛋嬌蘭專櫃人員
+                    每天只服務2個客人，提供更頂級的VIP紋繡服務。
                     <br />
                     <br />
-                    專業證書：
+                    經歷：化妝品保養專櫃人員11年經歷
+                    <IndentDiv>美容美體護膚人員8年經歷</IndentDiv>
+                    <IndentDiv>紋繡1年經歷</IndentDiv>
+                    <br />
+                    專業證書：英國TQUK國際證書
+                    <IndentDiv2>國家級丙級執照</IndentDiv2>
+                    <IndentDiv2>中華民國美容師協會證書</IndentDiv2>
+                    <IndentDiv2>中華民國美容協會飄眉師證書</IndentDiv2>
+                    <IndentDiv2>中華民國美容協會紋唇證書</IndentDiv2>
+                    <IndentDiv2>中華民國美容協會紋眼線證書</IndentDiv2>
                 </Introduction>
             </Content>
         </Container>
--- src/Components/Page/Question.js
+++ src/Components/Page/Question.js
@@ -65,7 +65,11 @@ function Question() {
                                 itm.isOpen ? "active-answer" : "close-answer"
                             }
                         >
-                            {itm.answer}
+                            {typeof itm.answer === "string"
+                                ? itm.answer
+                                : itm.answer?.map(
+                                      (el) => <div>{el}</div> || ""
+                                  )}
                         </AnswerField>
                     </DropDownItem>
                 ))}
@@ -80,25 +84,29 @@ const DUMMY_DATA = [
     {
         id: 1,
         question: "請問如何預約?",
-        answer: "可以直接透過官網老師可預約時間表預約,也可以透過LINE、臉書、IG帳號預約;另外也可電話預約。",
+        answer: "您可以登入會員後、點右側立即預約即可線上預約，省去利用LINE、臉書及IG等老師回復的時間；或是您也能透過電話預約。我們的客服時間是周六及周日的早上10點到下午6點。",
         isOpen: false,
     },
     {
         id: 2,
         question: "不是會員如何修改預約時間?",
-        answer: "不管是不是都沒得改。",
+        answer: "由於會員才享有修改預約的服務，非會員無法在線上即時修改預約，您可以透過電話並在客服時間時取消預約。",
         isOpen: false,
     },
     {
         id: 3,
-        question: "價格為什麼沒有一個確定的金額??",
-        answer: "因為你值得",
+        question: "霧眉、紋眼線或紋唇會很痛嗎?",
+        answer: "店內會在紋繡前先幫客戶敷上麻藥，藥效約20分鐘，加上Jamie老師輕柔的手法，全程只要放輕鬆睡完一覺 ( 約30P ~1小時 )，就能享有美麗的眉型 / 眼線 / 唇型喔!",
         isOpen: false,
     },
     {
         id: 4,
         question: "加入會員有什麼優惠?",
-        answer: "很多很多很多很多很多很多",
+        answer: [
+            "(1)加入會員享有一年內補色優惠價3500(原價5000元)",
+            "(2)親朋好友一起來!好康道相報，立即推薦親友，(該親友)即享現折500優惠",
+            "(3)當月壽星即享6折優惠",
+        ],
         isOpen: false,
     },
 ];
